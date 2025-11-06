ebe2170821e87a7ef0539c2727b5f2fcedeac189
Max Ustinov <maksimu@users.noreply.github.com>
2025-11-05 22:34:00 -0800

    Simplify setup.sh to work on Ubuntu 24.04 without PPA
    Killercoda uses Ubuntu 24.04 (noble) which has Ruby 3.2 in default repos.
    Remove Brightbox PPA which doesn't support Ubuntu 24.04 and causes 404 errors.
    
    Changes:
    - Remove software-properties-common installation
    - Remove add-apt-repository PPA step
    - Remove update-alternatives commands (not needed)
    - Install ruby3.2 directly from Ubuntu 24.04 default repos
    
    This fixes the gem installation failure and eliminates PPA 404 errors
    while maintaining Ruby 3.2.3 compatibility with KSM SDK 17.1.0.

--- ksm-sdk-ruby/setup.sh
+++ ksm-sdk-ruby/setup.sh
@@ -2,24 +2,13 @@
 
 echo "Installing Keeper Secrets Manager Ruby SDK..."
 
-# Install Ruby and dependencies (simple approach)
-apt update -qq && apt install -y -qq software-properties-common
-
-# Add Brightbox Ruby PPA for Ruby 3.2
-add-apt-repository -y ppa:brightbox/ruby-ng
-apt update -qq
-
-# Install Ruby 3.2
-apt install -y ruby3.2 ruby3.2-dev build-essential
-
-# Make Ruby 3.2 the default
-update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby3.2 1
-update-alternatives --install /usr/bin/gem gem /usr/bin/gem3.2 1
+# Install Ruby 3.2 and dependencies (available in Ubuntu 24.04 default repos)
+apt update -qq && apt install -y -qq ruby3.2 ruby3.2-dev build-essential
 
 # Install KSM gem
 gem install keeper_secrets_manager --version 17.1.0 --no-document
 
 echo "Installation completed!"
 echo "Testing installation..."
-ruby -e "require 'keeper_secrets_manager'; puts 'KSM Ruby SDK ' + KeeperSecretsManager::VERSION + ' ready!'" || echo "Installation completed but may need manual verification"
+ruby -e "require 'keeper_secrets_manager'; puts 'KSM Ruby SDK ' + KeeperSecretsManager::VERSION + ' ready!'" || echo "Installation may need verification"
 echo "Ready to start coding with KSM Ruby SDK!"
