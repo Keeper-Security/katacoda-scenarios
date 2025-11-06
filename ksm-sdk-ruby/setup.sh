bd234c951877fe0472777cef4b5e96401eac08cb
Max Ustinov <maksimu@users.noreply.github.com>
2025-11-05 23:12:48 -0800

    Remove unnecessary shebang from setup.sh
    Since Killercoda runs the script with 'bash setup.sh', the shebang is not needed
    and can cause issues with history expansion in some environments.

--- ksm-sdk-ruby/setup.sh
+++ ksm-sdk-ruby/setup.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-
 apt update -qq && apt install -y -qq ruby3.2 ruby3.2-dev build-essential && gem install keeper_secrets_manager --no-document && ruby -e "require 'keeper_secrets_manager'; puts 'KSM Ruby SDK ready!'" || echo "Installation completed. Run: gem install keeper_secrets_manager"
